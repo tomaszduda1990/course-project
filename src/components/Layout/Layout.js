aa44b8ad62cc7e11910043476aecdc75d0dc760e
Tomasz Duda <tduda@online.saba.com>
2020-10-26 17:33:04 +0100

    setup first steps

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const layout = (props) => (
+    <>
+        <div>Toolbar, Sidedrawer, Backdrop</div>
+        <main>{props.children}</main>
+    </>
+)
+
+export default layout;
