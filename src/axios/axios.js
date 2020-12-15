465d023cccf6a1a704dc2004a1f76efde58b3fa5
Tomasz Duda <tduda@online.saba.com>
2020-12-15 14:57:06 +0100

    added axios config to case

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,8 +1,16 @@
 import axios from 'axios'
 
-const instance = axios.create({
+export const instanceFirebase = axios.create({
     baseURL:
         'https://event-app-8a682-default-rtdb.europe-west1.firebasedatabase.app/',
 })
 
-export default instance
+const PIXABAY_API = '11047628-635bca23b99c10143c7630956'
+export const instancePixabay = axios.create({
+    baseURL: 'https://pixabay.com/api/',
+    params: {
+        key: PIXABAY_API,
+        image_type: 'photo',
+        safesearch: true,
+    },
+})
